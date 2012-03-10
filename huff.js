c194d543adf992a37282a4c7a6055732acdffef5
danx0r <danbmil99@gmail.com>
2012-03-10 13:16:57 -0800

    well that sucks

--- huff.js
+++ huff.js
@@ -16,7 +16,7 @@
  * takes a list of numbers, returns a list of bits
  */
 function huff(data) {
-    bits = []
+    bits = [];
     for (var i = 0; i < data.length; i++) {
         var v = data[i];
         switch (v) {
@@ -28,7 +28,7 @@ function huff(data) {
             default: {
                 if (Math.abs(v) <= 10) {
                     bits.concat([1, 1, 1, 0, v < 0 ? 1 : 0]);
-                    for (var j = 1; j < 16; j <<= 1) {
+                    for (var j = 1; j < 8; j <<= 1) {
                         bits.push((Math.abs(v) - 3) & j ? 1 : 0)
                     }
                 }
@@ -48,6 +48,54 @@ function huff(data) {
     return bits;
 }
 
+function deHuff(bits){
+    data = [];
+    var i = 0;
+    while (i < bits.length) {
+        if (bits[i++]) {
+            if (bits[i++]) {
+                if (bits[i++]) {
+                    if (bits[i++]) {                // 1111
+                        var sign = bits[i++];
+                        var n = 0;
+                        for (var j = 0; j < 10; j++) {
+                            n |= bits[i++];
+                            n <<= 1;
+                        }
+                        n += 10;
+                        if (sign) n = -n;
+                        data.push(n);
+                    }
+                    else {                          // 1110
+                        var sign = bits[i++];
+                        var n = 0;
+                        for (var j = 0; j < 3; j++) {
+                            n |= bits[i++];
+                            n <<= 1;
+                        }
+                        n += 3;
+                        if (sign) n = -n;
+                        data.push(n);
+                    }
+                }
+                else {                              // 110
+                    data.push(-2);
+                }
+            }
+            else {                                  // 10x
+                data.push(bits[i++] ? -1 : 2)
+            }
+        }
+        else {                                      // 0x
+            data.push(bits[i++]);
+        }
+    }
+    return data;
+}
+
 data=[0,1,1,-2,3,-7,14,-100];
+console.log("data:", data);
 bits = huff(data);
-console.log(bits);
+console.log("bits:", bits);
+data = deHuff(bits);
+console.log("data:", data);
