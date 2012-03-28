ec8c56b0f97c1e185907c58cceb993da45df88a5
danx0r <danbmil99@gmail.com>
2012-03-27 19:13:58 -0700

    circularity stddev

--- circularity.py
+++ circularity.py
@@ -1,6 +1,49 @@
+def calcDist(abc, xyz):
+    sumsq = 0.0
+    for i in range(3):
+        sumsq += (abc[i] - xyz[i]) ** 2
+    return sumsq ** 0.5
+
 f = open('log.csv')
 rl = f.readlines()
-print "count:", len(rl)
+count = float(len(rl))
+print "count:", count
+pts = []
 for r in rl:
     xyz = r.strip().split(',')
+    for i in range(3):
+        xyz[i] = float(xyz[i])
     print xyz
+    pts.append(xyz)
+
+avg = [0.0,0.0,0.0]
+for pt in pts:
+    for i in range(3):
+        avg[i] += pt[i]
+
+for i in range(3):
+    avg[i] /= count
+
+print "avg:", avg
+
+dist = []
+avdist = 0.0
+for pt in pts:
+    d = calcDist(avg, pt)
+    print "radius for this pt:", d
+    avdist += d
+    dist.append(d)
+
+avdist /= count
+
+print "average radius:", avdist
+
+sumsq = 0.0
+for pt in pts:
+    d = calcDist(avg, pt)
+    dif = d - avdist
+    sumsq += dif ** 2
+
+dev = (sumsq/count) ** 0.5
+
+print "standard deviation:", dev
