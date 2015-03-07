4373c7e6aed996962577e2f20695f35bd4e11f98
Rafael Cosman <rafaelCosman@gmail.com>
2015-03-07 15:26:38 -0800

    Started the new three state rule

---
+++ js/danAndRafaelNewThreeStateRule.js
@@ -0,0 +1,52 @@
+/*
+ * Dan and Rafale's new three state rule. Right now this is only implemeneted as 2-d rule.
+ */
+rule3state = function(grid, x,y,z, frame){
+	if ((frame+x+y+z) % 2 === 0)
+		return;
+
+	var done = false;
+	var newState;
+
+	if (grid.get(x, y, z+1) === 1) {
+		if (done) {
+			return;
+		} else {
+			done = true;
+		}
+
+		newState = grid.get(x+1, y, z+1);
+	}
+
+	if (grid.get(x, y, z-1) === 1) {
+		if (done) {
+			return;
+		} else {
+			done = true;
+		}
+
+		newState = grid.get(x-1, y, z-1);
+	}
+
+	if (grid.get(x+1, y, z) === 1) {
+		if (done) {
+			return;
+		} else {
+			done = true;
+		}
+
+		newState = grid.get(x+1, y, z-1);
+	}
+
+	if (grid.get(x-1, y, z) === 1) {
+		if (done) {
+			return;
+		} else {
+			done = true;
+		}
+
+		newState = grid.get(x-1, y, z+1);
+	}
+
+	return newState;
+}
