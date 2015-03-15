6928abc73813f9d7b8d01b8185576c200a2882e2
Rafael Cosman <rafaelCosman@gmail.com>
2015-03-15 09:25:16 -0700

    COMPLETED a conservative, 3-state rule
    Still only 2 dimensions, presumed but not known to be reversible.

--- js/rule3state.js
+++ js/rule3state.js
@@ -5,59 +5,87 @@ vector = function(x, y, z){
 	return {x:x, y:y, z:z};
 }
 
+add = function(oldVector, x, y, z) {
+	return vector(oldVector.x + x, oldVector.y + y, oldVector.z + z);
+}
+
 get = function(grid, getLocation) {
 	return grid.get(getLocation.x, getLocation.y, getLocation.z);
 }
 
-rule3state = function(grid, x,y,z, frame){
-	//console.log("grid.get(x, y, z): " + grid.get(x, y, z));
+XOR = function(a,b) {
+  return ( a || b ) && !( a && b );
+}
 
+getLocationToGetFrom = function(grid, x,y,z) {
+	var rotatorFound = false;
+	var rotatorLocation;
+	var deltaFromMeToRotatorLocation;
+
+	var possibleGuysToRotateMe = [
+		vector(+1, 0, 0),
+		vector(-1, 0, 0),
+		vector(0, 0, +1),
+		vector(0, 0, -1)
+	];
+
+	for (index in possibleGuysToRotateMe) {
+		var possibleGuyToRotateMe = possibleGuysToRotateMe[index];
+
+		if (get(grid, add(possibleGuyToRotateMe, x,y,z)) !== 0) {
+			if (rotatorFound) {
+				return;
+			}
+
+			rotatorLocation = add(possibleGuyToRotateMe, x,y,z);
+			deltaFromMeToRotatorLocation = possibleGuyToRotateMe;
+			rotatorFound = true;
+		}
+	}
+
+	if (!rotatorFound) {
+		return;
+	}
+	
+	var spacesThatNeedToBeEmpty = [
+		add(rotatorLocation, 2, 0, 0),
+		add(rotatorLocation, -2, 0, 0),
+		add(rotatorLocation, 0, 0, 2),
+		add(rotatorLocation, 0, 0, -2),
+		add(rotatorLocation, 1, 0, 1),
+		add(rotatorLocation, -1, 0, -1),
+		add(rotatorLocation, 1, 0, -1),
+		add(rotatorLocation, -1, 0, 1)
+	];
+
+	for (var index in spacesThatNeedToBeEmpty) {
+		var spaceThatNeedsToBeEmpty = spacesThatNeedToBeEmpty[index]
+		if (get(grid, spaceThatNeedsToBeEmpty) !== 0) {
+			return;
+		}
+	}
+	
+	rotatorState = get(grid, rotatorLocation);
+	if (rotatorState === 1)
+		return add(vector(
+			deltaFromMeToRotatorLocation.z,
+			0,
+			-deltaFromMeToRotatorLocation.x),
+			rotatorLocation.x,rotatorLocation.y,rotatorLocation.z);
+	if (rotatorState === -1)
+		return add(vector(
+			-deltaFromMeToRotatorLocation.z,
+			0,
+			deltaFromMeToRotatorLocation.x),
+			rotatorLocation.x,rotatorLocation.y,rotatorLocation.z);
+}
+
+rule3state = function(grid, x,y,z, frame){
 	if ((frame+x+y+z) % 2 === 0)
 		return;
 
-	var done = false;
-	var getLocation;
+	var locationToGetFrom = getLocationToGetFrom(grid, x,y,z);
 
-	if (grid.get(x, y, z+1) === 1 || grid.get(x+1, y, z) === -1) {
-		if (done) {
-			return;
-		} else {
-			done = true;
-		}
-
-		getLocation = vector(x+1, y, z+1);
-	}
-
-	if (grid.get(x, y, z-1) === 1 || grid.get(x-1, y, z) === -1) {
-		if (done) {
-			return;
-		} else {
-			done = true;
-		}
-
-		getLocation = vector(x-1, y, z-1);
-	}
-
-	if (grid.get(x+1, y, z) === 1 || grid.get(x, y, z-1) === -1) {
-		if (done) {
-			return;
-		} else {
-			done = true;
-		}
-
-		getLocation = vector(x+1, y, z-1);
-	}
-
-	if (grid.get(x-1, y, z) === 1 || grid.get(x, y, z+1) === -1) {
-		if (done) {
-			return;
-		} else {
-			done = true;
-		}
-
-		getLocation = vector(x-1, y, z+1);
-	}
-
-	if(getLocation)
-		return get(grid, getLocation);
+	if (locationToGetFrom)
+		return get(grid, locationToGetFrom);
 }
